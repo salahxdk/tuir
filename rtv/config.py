de1d06e33c7247ba9159d11e5d51fb72201622e5
John Helmert <jchelmertt3@gmail.com>
2019-06-06 19:31:42 -0500

    Replace deprecated ConfigParser method
    ConfigParser.readfp() -> ConfigParser.read_file()

--- rtv/config.py
+++ rtv/config.py
@@ -250,7 +250,7 @@ class Config(object):
         config = configparser.ConfigParser()
         if os.path.exists(filename):
             with codecs.open(filename, encoding='utf-8') as fp:
-                config.readfp(fp)
+                config.read_file(fp)
 
         return cls._parse_rtv_file(config)
 
